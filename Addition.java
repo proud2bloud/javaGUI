04430d59e9c6258103c463ffa7b3df237bbe5317
Hem Joshi <nirvanacobain.hem@gmail.com>
2020-08-07 18:11:42 +0200

    Pushed Addition.java to remote
    The file asks for two integers from the user in two different Panes and displays the sum of two integers in a third pane.

---
+++ Addition.java
@@ -0,0 +1,19 @@
+import javax.swing.JOptionPane;
+public class Addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//obtain user input from JOptionPane input dialogs
+		String firstNumber = JOptionPane.showInputDialog("Enter first integer: ");
+		String secondNumber = JOptionPane.showInputDialog("Enter second integer: ");
+		
+		int number1 = Integer.parseInt(firstNumber);
+		int number2 = Integer.parseInt(secondNumber);
+		
+		int sum = number1 + number2;
+		JOptionPane.showMessageDialog(null, "The sum is " + sum, "Sum of two integers", JOptionPane.PLAIN_MESSAGE);
+		
+
+	}
+
+}
